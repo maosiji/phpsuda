d9355110c340ee348fc9b3d3a532cec362d203e3
maosiji <code@maosiji.cn>
2024-09-20 17:35:49 +0800

    测试

---
+++ a.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * author               : 无柳先生
+ * url                  : wuliuxiansheng.com
+ * email                : wuliuxiansheng@qq.com
+ * date                 : 2024-09-20 17:34
+ * update               : 
+ * project              : phpsuda
+ * official website     : wpsuda.cn
+ * official name        : WP速搭
+ * description          : 这家伙很懒，没有写介绍
+ * read me              : 感谢您使用 WP速搭 的产品。您的支持，是我们最大的动力；您的反对，是我们最大的阻力
+ * remind               ：使用盗版，存在风险；支持正版，将会有跟多的产品与您见面
+ */
